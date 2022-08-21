1408f89468c007b185cfa175ab8cdcb8e5d665ba
DorisLiu <dorisliu113@gmail.com>
2022-08-21 12:35:12 +1000

    add homeRoutes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -4,11 +4,9 @@ const { User } = require('../../models');
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
-
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-
       res.status(200).json(userData);
     });
   } catch (err) {

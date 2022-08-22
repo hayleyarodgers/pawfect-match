08e349c968af9b86da1c34e902c442ea67993e4a
DorisLiu <dorisliu113@gmail.com>
2022-08-22 15:53:47 +1000

    fix conflicts

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine with custom helpers
-// const hbs = exphbs.create({ helpers });
+// Set up Handlebars.js engine
+const hbs = exphbs.create();
 
 const sess = {
 	secret: 'Super secret secret',

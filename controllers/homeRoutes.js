48cf4b1f7844256f8934dcb2c03416141a02ac17
Vanessa Bloom <ness.jade@outlook.com>
2022-08-23 15:54:00 +1000

    Handlebars html + styling for pc
    I have finished the html and bootstrap for the login and signup page. If we have time, we should go back and finalize the responsive styling as that could do with some work. We can now navigate from the homepage to the login/signup page :)

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,143 +4,130 @@ const withAuth = require('../utils/auth');
 
 //render homepage
 router.get('/', async (req, res) => {
-  res.render('homepage');
+	res.render('homepage');
 });
 
 //render the post for adoption page
 router.get('/postforadoption', async (req, res) => {
-  res.render('postForAdoption');
+	res.render('postForAdoption');
 });
 
 //render pets adoption page
 router.get('/adoptpets', async (req, res) => {
-  try {
-    const petData = await Pet.findAll();
-    if (!petData) {
-      res.status(400).json({ message: 'can not find pet data' });
-      return;
-    }
-    const pets = await petData.map(item => item.get({ plain: true }))
-    res.render('adoptpets', {
-      pets,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+	try {
+		const petData = await Pet.findAll();
+		if (!petData) {
+			res.status(400).json({ message: 'can not find pet data' });
+			return;
+		}
+		const pets = await petData.map((item) => item.get({ plain: true }));
+		res.render('adoptpets', {
+			pets,
+			logged_in: req.session.logged_in,
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 //render cat list
 router.get('/adoptpets/cat', async (req, res) => {
-  try {
-    const catsData = await Pet.findAll({ where: { type: 'cat' } });
-    if (!catsData) {
-      res.status(400).json({ message: 'can not find cat data' });
-      return;
-    }
-    const cats = await catsData.map(item => item.get({ plain: true }))
-    res.render('catList', {
-      cats,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+	try {
+		const catsData = await Pet.findAll({ where: { type: 'cat' } });
+		if (!catsData) {
+			res.status(400).json({ message: 'can not find cat data' });
+			return;
+		}
+		const cats = await catsData.map((item) => item.get({ plain: true }));
+		res.render('catList', {
+			cats,
+			logged_in: req.session.logged_in,
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 //render each cat page
 router.get('/adoptpet/cat/:id', async (req, res) => {
-  try {
-    const catData = await Pet.findByPk(req.params.id,
-      {
-        attributes: { exclude: ['password'] },
-        include: [{ model: User }],
-      }
-    );
-    const cat = catData.get({ plain: true });
+	try {
+		const catData = await Pet.findByPk(req.params.id, {
+			attributes: { exclude: ['password'] },
+			include: [{ model: User }],
+		});
+		const cat = catData.get({ plain: true });
 
-    res.render('cat', {
-      ...cat,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+		res.render('cat', {
+			...cat,
+			logged_in: req.session.logged_in,
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 //render dog list
 router.get('/adoptpets/dog', async (req, res) => {
-  try {
-    const dogsData = await Pet.findAll({ where: { type: 'dog' } });
-    if (!dogsData) {
-      res.status(400).json({ message: 'can not find cat data' });
-      return;
-    }
-    const dogs = await dogsData.map(item => item.get({ plain: true }))
-    res.render('dogList', {
-      dogs,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+	try {
+		const dogsData = await Pet.findAll({ where: { type: 'dog' } });
+		if (!dogsData) {
+			res.status(400).json({ message: 'can not find cat data' });
+			return;
+		}
+		const dogs = await dogsData.map((item) => item.get({ plain: true }));
+		res.render('dogList', {
+			dogs,
+			logged_in: req.session.logged_in,
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 //render each dog page
 router.get('/adoptpet/dog/:id', async (req, res) => {
-  try {
-    const dogData = await Pet.findByPk(req.params.id,
-      {
-        attributes: { exclude: ['password'] },
-        include: [{ model: User }],
-      }
-    );
-    const dog = dogData.get({ plain: true });
+	try {
+		const dogData = await Pet.findByPk(req.params.id, {
+			attributes: { exclude: ['password'] },
+			include: [{ model: User }],
+		});
+		const dog = dogData.get({ plain: true });
 
-    res.render('dog', {
-      ...dog,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+		res.render('dog', {
+			...dog,
+			logged_in: req.session.logged_in,
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 //render dashboard page
 router.get('/dashboard', withAuth, async (req, res) => {
-  try {
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Pet }],
-    });
+	try {
+		const userData = await User.findByPk(req.session.user_id, {
+			attributes: { exclude: ['password'] },
+			include: [{ model: Pet }],
+		});
 
-    const user = userData.get({ plain: true });
+		const user = userData.get({ plain: true });
 
-    res.render('dashboard', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+		res.render('dashboard', {
+			...user,
+			logged_in: true,
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 //render login page
 router.get('/login', (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect('/pets');
-    return;
-  }
-  res.render('login');
-});
-
-//render signup page
-router.get('/signup', (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect('/dashboard');
-    return;
-  }
-  res.render('signup');
+	if (req.session.logged_in) {
+		res.redirect('/pets');
+		return;
+	}
+	res.render('login');
 });
 
 module.exports = router;

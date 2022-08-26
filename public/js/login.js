4e8da8dbe231d57ec466146a6a2c3856b1c11710
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-08-26 14:49:12 +1000

    Fix username vs email typo

--- public/js/login.js
+++ public/js/login.js
@@ -2,7 +2,7 @@ const loginFormHandler = async (event) => {
 	event.preventDefault();
 
 	// Collect values from the login form
-	const email = document.querySelector('#username-login').value.trim();
+	const email = document.querySelector('#email-login').value.trim();
 	const password = document.querySelector('#password-login').value.trim();
 
 	if (email && password) {
